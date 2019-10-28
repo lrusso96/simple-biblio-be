e4806225e6fee2bc4d7081c26c977505d9374787
luigi <russo.1699981@studenti.uniroma1.it>
2019-10-28 10:59:11 +0100

    user model and spec

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+
+  # Validations
+  validates_presence_of :name, :email, :password_digest
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_090808) do
+ActiveRecord::Schema.define(version: 2019_10_28_090153) do
 
   create_table "ebooks", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
