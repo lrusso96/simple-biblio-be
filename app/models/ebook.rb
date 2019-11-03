cc379afeff91bbcff5854b8296a6acd5d35a7b1f
luigi <russo.1699981@studenti.uniroma1.it>
2019-11-03 10:29:42 +0100

    improve ebook json serializer

--- app/models/ebook.rb
+++ app/models/ebook.rb
@@ -4,15 +4,19 @@ class Ebook < ApplicationRecord
   belongs_to :provider
   has_many :ebook_readers
   has_many :users, through: :ebook_readers
+  acts_as_votable
 
   # validations
   validates_presence_of :author, :title, :rel_id
   validates :rel_id, uniqueness: { scope: :provider }
 
   def as_json(_options = {})
-    { title: title,
+    { id: rel_id,
+      title: title,
       author: author,
       provider: provider,
-      id: rel_id }
+      ratings: cached_votes_total,
+      rating_avg: cached_weighted_average
+    }
   end
 end
